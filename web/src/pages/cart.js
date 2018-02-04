4f0508b66617f862425ffeb83d53ab8d03816373
Hosein Khansari <hrk.x11@gmail.com>
2018-02-04 20:00:42 +0330

    ✔️ complete and debug cart, but stateless...

--- web/src/pages/cart.js
+++ web/src/pages/cart.js
@@ -3,9 +3,13 @@ import Helmet from 'react-helmet';
 import AppBar from 'material-ui/AppBar';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
+import TouchRipple from 'material-ui/internal/TouchRipple';
 import {Link} from 'react-router-dom';
 import {palette, Space, slicePrice, numToFA} from '../utils/';
 import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
 import {
     Table, TableRow, TableHeader, TableHeaderColumn, TableBody, TableRowColumn
 } from 'material-ui/Table';
@@ -33,9 +37,13 @@ export default class ShoppingCart extends Component {
                 price: 41000,
                 amount: 4
             }
-        ]
+        ],
+        confirmDialogOpen: false
     }
     render(){
+        const closeD = () => {
+            this.setState({confirmDialogOpen: false})
+        }
         return (
             <Fragment>
                 <Helmet>
@@ -45,70 +53,60 @@ export default class ShoppingCart extends Component {
                     <AppBar style={{backgroundColor: 'transparent'}} title='سبد خرید' zDepth={0} iconElementLeft={<Link to='/'><IconButton><FontIcon className='mdi' color={palette.primary3Color}>arrow_forward</FontIcon></IconButton></Link>} />
                 </Paper>
                 <div className='col-xs-12 col-md-8' style={{position: 'relative', top: -70, float: 'none', margin: '0 auto'}}>
-                    <Paper style={{overflow: 'auto', padding: 15}} zDepth={1}>
-                        <table className='cart-main'>
-                            <thead>
-                                <tr>
-                                    <th>نام</th>
-                                    <th>تعداد</th>
-                                    <th>قیمت واحد</th>
-                                    <th>قیمت کل</th>
-                                    <th>فعالیت ها</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {/* <tr>
-                                    <td>آچار فرانسه</td>
-                                    <td>4</td>
-                                    <td>20000</td>
-                                    <td>80000</td>
-                                    <td>
-                                        <IconButton tooltipPosition='top-center' tooltip='حذف آیتم از سبد خرید'>
-                                    <FontIcon className='mdi' color='#f44336'>
-                                    delete
-                                    </FontIcon>
-                                        </IconButton>
-                                        <IconButton tooltipPosition='top-center' tooltip='افزایش تعداد کالا'>
-                                    <FontIcon className='mdi' color={palette.accent1Color}>
-                                    add_circle_outline
-                                    </FontIcon>
-                                        </IconButton>
-                                        <IconButton tooltipPosition='top-center' tooltip='کاهش تعداد کالا'>
-                                    <FontIcon className='mdi' color={palette.accent2Color}>
-                                    remove_circle_outline
-                                    </FontIcon>
-                                        </IconButton>
-                                    </td>
-                                </tr> */}
-                                {this.state.items.map(e => <tr>
-                                    <td>{e.name}</td>
-                                    <td>{numToFA(e.amount)}</td>
-                                    <td>{slicePrice(numToFA(e.price))} تومان</td>
-                                    <td>{slicePrice(numToFA(e.price * e.amount))} تومان</td>
-                                    <td className='actions'>
-                                        <div>
-                                            <IconButton tooltipPosition='top-center' tooltip='حذف آیتم از سبد خرید'>
-                                                <FontIcon className='mdi' color='#f44336'>
-                                                    delete
-                                                </FontIcon>
-                                            </IconButton>
-                                            <IconButton tooltipPosition='top-center' tooltip='افزایش تعداد کالا'>
-                                                <FontIcon className='mdi' color={palette.accent1Color}>
-                                                    add_circle_outline
-                                                </FontIcon>
-                                            </IconButton>
-                                            <IconButton tooltipPosition='top-center' tooltip='کاهش تعداد کالا'>
-                                                <FontIcon className='mdi' color={palette.accent2Color}>
-                                                    remove_circle_outline
-                                                </FontIcon>
-                                            </IconButton>
-                                        </div>
-                                    </td>
-                                </tr>)}
-                            </tbody>
-                        </table>
+                    <Paper style={{padding: 15}} zDepth={1}>
+                        <div style={{overflow: 'auto'}}>
+                            <table className='cart-main'>
+                                <thead>
+                                    <tr>
+                                        <th>نام</th>
+                                        <th>تعداد</th>
+                                        <th>قیمت واحد</th>
+                                        <th>قیمت کل</th>
+                                        <th>فعالیت ها</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.items.map(e => <tr>
+                                        <td>{e.name}</td>
+                                        <td>{numToFA(e.amount)}</td>
+                                        <td>{slicePrice(numToFA(e.price))} تومان</td>
+                                        <td>{slicePrice(numToFA(e.price * e.amount))} تومان</td>
+                                        <td className='actions'>
+                                            <div>
+                                                <IconButton tooltipPosition='top-center' tooltip='حذف آیتم از سبد خرید'>
+                                                    <FontIcon className='mdi' color='#f44336'>
+                                                        delete
+                                                    </FontIcon>
+                                                </IconButton>
+                                                <IconButton tooltipPosition='top-center' tooltip='افزایش تعداد کالا'>
+                                                    <FontIcon className='mdi' color={palette.accent1Color}>
+                                                        add_circle_outline
+                                                    </FontIcon>
+                                                </IconButton>
+                                                <IconButton tooltipPosition='top-center' tooltip='کاهش تعداد کالا'>
+                                                    <FontIcon className='mdi' color={palette.accent2Color}>
+                                                        remove_circle_outline
+                                                    </FontIcon>
+                                                </IconButton>
+                                            </div>
+                                        </td>
+                                    </tr>)}
+                                </tbody>
+                            </table>
+                        </div>
+                        <Space height={15} />
+                        <div style={{textAlign: 'center'}}>
+                            <RaisedButton onClick={() => {
+                                this.setState({confirmDialogOpen: true})
+                            }} secondary label='نهایی سازی خرید' icon={<FontIcon className='mdi'>done</FontIcon>} />
+                        </div>
                     </Paper>
                 </div>
+
+                <Dialog title='تأئید نهایی سازی خرید' open={this.state.confirmDialogOpen} onRequestClose={closeD} actions={[<FlatButton secondary={true} onClick={closeD}>بله</FlatButton>, <FlatButton secondary={true} onClick={closeD}>خیر</FlatButton>]}>
+                    آیا از نهایی سازی خرید خود اطمینان دارید؟
+                    <b>این کار غیرقابل بازگشت است!</b>
+                </Dialog>
             </Fragment>
         )
     }
