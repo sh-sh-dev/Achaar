6eff1afd971b50d90977ff1b3c661757402fd162
Hosein Khansari <hrk.x11@gmail.com>
2018-02-12 21:55:24 +0330

    ⭐ create order-finalization page (no review part!)

--- web/src/pages/orderf.jsx
+++ web/src/pages/orderf.jsx
@@ -1,12 +1,14 @@
 import React, {Fragment as F} from 'react';
-import {Redirect} from 'react-router-dom';
+import {Redirect, Link} from 'react-router-dom';
 import {palette, cookie, Space, BlankLink} from '../utils/';
 import AppBar from 'material-ui/AppBar';
 import FontIcon from 'material-ui/FontIcon';
 import IconButton from 'material-ui/IconButton';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import {
     Step,
     Stepper,
@@ -23,9 +25,26 @@ const GridInput = (props) => {
         borderBottom = props.borderBottom;
     }
     return (
-        <div className={`col-xs-12 col-md-${props.colSize || 12}`} style={{padding: 16, display: 'flex', alignItems: 'center', borderBottom: borderBottom ? '1px solid #dedede' : 'none'}}>
-            <div style={{paddingLeft: 16, whiteSpace: 'nowrap'}}><i className='mdi' style={{color: palette.accent3Color}}>{props.icon.split(' ').join('_')}</i>  <b>{props.placeHolder}</b></div>
-            <TextField {...props.inputProps} style={{margin: 0}} fullWidth underlineShow={false} hintText={props.placeHolder} defaultValue={props.defaultValue} />
+        <div
+            className={`col-xs-12 col-md-${props.colSize || 12}`}
+            style={{padding: 16, display: 'flex', alignItems: 'center', borderBottom: borderBottom ? '1px solid #dedede' : 'none'}}>
+            <div style={{paddingLeft: 16, whiteSpace: 'nowrap'}}>
+                <i
+                    className='mdi'
+                    style={{color: palette.accent3Color}}>
+                    {props.icon.split(' ').join('_')}
+                </i>
+                <b>
+                    {' ' + props.placeHolder}
+                </b>
+            </div>
+            <TextField
+                {...props.inputProps}
+                style={{margin: 0}}
+                fullWidth
+                underlineShow={false}
+                hintText={props.placeHolder}
+                defaultValue={props.defaultValue} />
         </div>
     )
 }
@@ -33,7 +52,7 @@ const GridInput = (props) => {
 class OrderFinalization extends React.Component {
     auth = cookie.get('AS_AUTH')
     state = {
-        activeStep: 0,
+        activeStep: 1,
         mobile: window.innerWidth < 768
     }
     componentDidMount() {
@@ -49,21 +68,43 @@ class OrderFinalization extends React.Component {
         switch (this.state.activeStep) {
             case 0:
                 return <F>
-                    <h3 style={{margin: 16}}>مشخصات گیرنده</h3>
-                    <GridInput inputProps={{autoFocus: true}} icon='person' placeHolder='نام گیرنده' defaultValue="اصغر اکبری" />
-                    <GridInput placeHolder='آدرس گیرنده' icon='home' defaultValue='همونجا' />
-                    <GridInput placeHolder='شماره تلفن ثابت' borderBottom={this.state.mobile} icon='call' defaultValue='44514365' colSize={5} />
-                    <GridInput borderBottom={false} placeHolder='شماره تلفن همراه' icon='call' defaultValue='09357778351' colSize={7} />
+                    <h3 style={{margin: 16}}>
+                        مشخصات گیرنده
+                    </h3>
+                    <GridInput
+                        inputProps={{autoFocus: true}}
+                        icon='person'
+                        placeHolder='نام گیرنده'
+                        defaultValue="اصغر اکبری" />
+                    <GridInput
+                        placeHolder='آدرس گیرنده'
+                        icon='home'
+                        defaultValue='همونجا' />
+                    <GridInput
+                        placeHolder='شماره تلفن ثابت'
+                        borderBottom={this.state.mobile}
+                        icon='call'
+                        defaultValue='44514365'
+                        colSize={5} />
+                    <GridInput
+                        borderBottom={false}
+                        placeHolder='شماره تلفن همراه'
+                        icon='call'
+                        defaultValue='09357778351'
+                        colSize={7} />
                     <Space height={16} />
                     <div style={{padding: 16, textAlign: 'center'}}>
-                        <RaisedButton onClick={() => {
+                        <RaisedButton
+                            onClick={() => {
                                 this.setState({activeStep: 1})
-                            }} label='تایید اطلاعات' primary={true} />
+                            }}
+                            label='تایید اطلاعات'
+                            primary={true} />
                     </div>
                 </F>;
             case 1:
                 return <div style={{padding: 16}}>
-                    <h3 style={{marginTop: 0}}>شیوه ارسال</h3>
+                    <h3 style={{marginTop: 0}}>انتخاب پیک</h3>
                     <RadioButtonGroup name='peyk' defaultSelected='alopeyk'>
                         <RadioButton
                             value='alopeyk' label={
@@ -82,6 +123,37 @@ class OrderFinalization extends React.Component {
                                 <F>ارسال کالا با <b>پیک پیشتاز</b></F>
                             } />
                     </RadioButtonGroup>
+                    <Divider style={{marginTop: 16}} />
+                    <h3>نحوه پرداخت</h3>
+                    <RadioButtonGroup name='payment_method' defaultSelected='online'>
+                        <RadioButton
+                            style={{
+                                marginBottom: 16
+                            }}
+                            value='naghd'
+                            label='پرداخت نقدی در مقصد' />
+                        <RadioButton
+                            value='online'
+                            label='پرداخت آنلاین' />
+                    </RadioButtonGroup>
+                    <Space height={15} />
+                    <div style={{textAlign: 'center'}}>
+                        <FlatButton
+                            label='بازگشت' onClick={
+                                () => {
+                                    this.setState({
+                                        activeStep: 0
+                                    });
+                                }
+                            } style={{marginRight: 8}} />
+                        <RaisedButton onClick={
+                                () => {
+                                    this.setState({
+                                        activeStep: 2
+                                    })
+                                }
+                            } label='تایید' primary />
+                    </div>
                 </div>
             case 2:
                 return <div>همین دیگه.</div>;
@@ -93,7 +165,7 @@ class OrderFinalization extends React.Component {
                 <Paper
                     rounded={false}
                     style={{
-                        backgroundColor: palette[`primary${this.state.activeStep + 1}Color`],
+                        backgroundColor: palette[`accent${this.state.activeStep + 1}Color`],
                         height: 256
                     }}>
                     <AppBar
@@ -102,21 +174,25 @@ class OrderFinalization extends React.Component {
                         style={{
                             backgroundColor: 'transparent'
                         }}
-                        iconElementLeft={<F></F>} />
+                        iconElementLeft={
+                            <IconButton>
+                                <FontIcon className='mdi'>arrow_forward</FontIcon>
+                            </IconButton>
+                    } />
                 </Paper>
                 <Paper
-                rounded={false}
-                zDepth={5}
-                style={{
-                    position: 'fixed',
-                    bottom:0,
-                    left:0,
-                    right: 0,
-                    zIndex: 1100,
-                    height: 64,
-                    display:'flex',
-                    alignItems: 'center'
-                }}>
+                    rounded={false}
+                    zDepth={5}
+                    style={{
+                        position: 'fixed',
+                        bottom:0,
+                        left:0,
+                        right: 0,
+                        zIndex: 1100,
+                        height: 35,
+                        display:'flex',
+                        alignItems: 'center'
+                    }}>
                     <Stepper ref={
                             (e) => {this.steps = e}
                         } style={{width: '100%'}} activeStep={this.state.activeStep}>
@@ -135,7 +211,7 @@ class OrderFinalization extends React.Component {
                     <div className='col-xs-12 col-md-8' style={{float: 'none', margin: '0 auto'}}>
                         <Paper style={{
                                 overflow: 'auto',
-                                maxHeight: 'calc(100vh - 256px + 128px - 64px - 16px)'
+                                maxHeight: 'calc(100vh - 256px + 128px - 35px - 16px)'
                             }} className='clear' zDepth={2}>
                             {this.getStuff()}
                         </Paper>
