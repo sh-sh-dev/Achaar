1d9832f6349c803ef31e11ebe39c9052b820f346
Hosein Khansari <hrk.x11@gmail.com>
2018-02-13 16:19:22 +0330

    🎨 restyle error 404

--- web/src/pages/err404.jsx
+++ web/src/pages/err404.jsx
@@ -5,14 +5,23 @@ import {palette, Space} from '../utils/'
 
 export default function Err404() {
     return (
-        <div className='load-indic'>
-            <i className='mdi' style={{color: '#f44336', fontSize: 80}}>cloud_off</i>
+        <div
+            className='load-indic'
+            style={{backgroundColor: palette.primary1Color}}>
+            <i
+                className='mdi'
+                style={{opacity: .5, fontSize: 80}}>cloud_off</i>
             <div style={{fontSize: '1.8em', margin: '.67rem 0', textAlign: 'center', fontWeight: 300}}>
-                <b>متأسفانه صفحه درخواست شده وجود ندارد.</b>
+                <b>
+                    متأسفانه صفحه درخواست شده وجود ندارد.
+                </b>
             </div>
             <Space height={15} />
             <Link to='/'>
-                <RaisedButton backgroundColor={palette.accent2Color} labelColor='#fff' label='خروج به صفحه اصلی' />
+                <RaisedButton
+                    backgroundColor={palette.accent2Color}
+                    labelColor='#fff'
+                    label='خروج به صفحه اصلی' />
             </Link>
         </div>
     )
