33b9be71c32790f59173271eb494b69b8cbc545b
Shaygan Shokrollahi <shayganshok0@gmail.com>
2018-02-22 14:24:52 +0330

    :sparkles: addComment score

--- api/addComment.php
+++ api/addComment.php
@@ -6,8 +6,7 @@ $Data = getData();
 $Token = $db->real_escape_string(Clean($Data["token"]));
 $Title = $db->real_escape_string(Clean($Data["title"]));
 $Text = $db->real_escape_string(Clean($Data["text"]));
-//$Score = (int)Clean($Data["score"]);
-$Score = 100;
+$Score = (int)Clean($Data["score"]);
 $Product = (int)Clean($Data["product"]);
 
 if (!CheckToken($Data["token"])) die(Response("توکن معتبر نیست",false,-2));
@@ -20,10 +19,14 @@ if (!CheckProduct($Product)) {
     changeUser($User,'canComment',0);
     die(Response("محصول مورد نظر وجود ندارد",false,-800));
 }
-if (!$canComment) die(Response("شما اجازه درج نظر را ندارید",false,-801));
+if ($Score < 1 || $Score > 5) {
+    changeUser($User,'canComment',0);
+    die(Response("امتیاز معتبر نیست",false,-801));
+}
+if (!$canComment) die(Response("شما اجازه درج نظر را ندارید",false,-802));
 
 $addComment = $db->query("INSERT INTO `comments` (`user`,`product`,`title`,`text`,`score`,`date`) VALUES ($User,$Product,'$Title','$Text',$Score,UNIX_TIMESTAMP())");
 
-if (!$addComment) die(Response("ثبت نظر مقدور نبود",false,-802));
+if (!$addComment) die(Response("ثبت نظر مقدور نبود",false,-803));
 
 Response("نظر ثبت شد",true,null,true);
--- api/getCart.php
+++ api/getCart.php
@@ -1,2 +1,7 @@
 <?
+include "functions.php";
 
+$Data = getData();
+
+$Token = Clean($Data["token"]);
+if (!CheckToken($Token)) die(Response("توکن معتبر نیست",false,-2));
