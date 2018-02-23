be2c3915bfd070c00ee895d0922726ab50b6e3e2
Shaygan Shokrollahi <shayganshok0@gmail.com>
2018-02-23 21:12:55 +0330

    :sparkles: addCart and getCart init

--- api/getCart.php
+++ api/getCart.php
@@ -5,3 +5,7 @@ $Data = getData();
 
 $Token = Clean($Data["token"]);
 if (!CheckToken($Token)) die(Response("توکن معتبر نیست",false,-2));
+
+$User = getToken($Token,'user');
+
+$getCart = $db->query("SELECT * FROM `cart` WHERE `user`='$User'");
