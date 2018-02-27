bcc49134fb71fe6c27141e593c0caf2546658afb
Hosein Khansari <hrk.x11@gmail.com>
2018-02-27 16:47:00 +0330

    🔧 config

--- web/config/webpack.config.dev.js
+++ web/config/webpack.config.dev.js
@@ -151,6 +151,10 @@ module.exports = {
               cacheDirectory: true,
             },
           },
+          {
+            test: /\.rt/,
+            loaders: ["babel-loader?presets[]=env", "react-templates-loader?modules=es6"]
+          },
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
