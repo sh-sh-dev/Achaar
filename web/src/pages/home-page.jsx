ca03fb5ffd99a7f44db7a893da7d009438413fa6
Hosein Khansari <hrk.x11@gmail.com>
2018-03-05 17:42:43 +0330

    🎨 homepage improvement

--- web/src/pages/home-page.jsx
+++ web/src/pages/home-page.jsx
@@ -1,18 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import MenuItem from 'material-ui/MenuItem';
+import Paper from 'material-ui/Paper';
 import SwipeableViews from 'react-swipeable-views';
-import { virtualize } from 'react-swipeable-views-utils';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import Badge from 'material-ui/Badge';
 import {Link} from 'react-router-dom';
 import qs from 'querystringify';
 import FontIcon from 'material-ui/FontIcon';
-import {MulitshSelf, MulitshItem, TypeText, Space, palette, cookie, validateCookie, numToFA} from '../utils/' ;
+import RaisedButton from 'material-ui/RaisedButton';
+import {MulitshSelf, MulitshItem, TypeText, Space, palette, validateCookie, numToFA} from '../utils/' ;
 import Helmet from 'react-helmet';
 
 export default class HomePage extends React.Component {
@@ -33,6 +34,92 @@ export default class HomePage extends React.Component {
             searchDialogOpen: false
 		})
 	}
+    get bar(){
+        let color = '#fff';
+        return (
+            <AppBar style={{position: 'fixed', top: 0, backgroundColor: 'transparent'}} iconElementLeft={<IconButton><FontIcon className='mdi' color={color}>menu</FontIcon></IconButton>} zDepth={0} title='' iconElementRight={<React.Fragment>
+                <IconButton onClick={() => {
+                    this.setState({
+                        searchDialogOpen: true
+                    })
+                }} tooltip='جستجو'>
+                    <FontIcon color={color} className="mdi">search</FontIcon>
+                </IconButton>
+                {this.auth && <Link to='/cart'>
+                    <IconButton tooltip='مشاهده سبد خرید'>
+                        <FontIcon className='mdi' color={color}>shopping_cart</FontIcon>
+                        <Badge
+                            badgeContent={numToFA(5)}
+                            badgeStyle={{top: -36, left: -12}}
+                            secondary={true}>
+                        </Badge>
+                    </IconButton>
+                </Link>
+                }
+                <IconMenu iconButtonElement={
+                        <IconButton tooltipPosition='bottom-left' tooltip='حساب کاربری'>
+                            <FontIcon color={color} className='mdi'>
+                                person
+                            </FontIcon>
+                        </IconButton>
+                    } targetOrigin={{horizontal: 'right', vertical: 'top'}} anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
+                    {!this.auth ?
+                        <React.Fragment>
+                            <Link to='/signup'>
+                                <MenuItem primaryText='ثبت نام' />
+                            </Link>
+                            <Link to='/signin'>
+                                <MenuItem primaryText="ورود به حساب" />
+                            </Link>
+                        </React.Fragment>
+                    :
+                    <React.Fragment>
+                        <Link to='/account/logout'>
+                            <MenuItem primaryText='خروج از حساب' />
+                        </Link>
+                        <Link to='/account/delete-account'>
+                            <MenuItem primaryText='حذف حساب کاربری' />
+                        </Link>
+                    </React.Fragment>
+                    }
+                </IconMenu>
+            </React.Fragment>} onLeftIconButtonClick={() => this.setState(prev => ({drawerOpen: true}))} />
+        )
+    }
+    get searchDialog(){
+        return (
+            <Dialog open={this.state.searchDialogOpen} onRequestClose={this.closeSearchDialog} title='جستجو در آچار'
+                contentStyle={{
+                    width: '96vw',
+                    maxWidth: 720
+                }}>
+                <form onSubmit={this.handleSearch} style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    flexDirection: 'row-reverse'
+                }}>
+                <IconButton type="submit" tabIndex={-1} style={{marginLeft: 8}}>
+                    <FontIcon className="mdi">search</FontIcon>
+                </IconButton>
+                <TextField ref='searchField' autoFocus={this.state.searchDialogOpen} fullWidth autoComplete='off' hintText='جستجو کنید...' name='query' />
+                <IconButton tabIndex={-1} type="button" style={{marginRight: 8}} onClick={this.closeSearchDialog}>
+                    <FontIcon className="mdi">close</FontIcon>
+                </IconButton>
+            </form>
+        </Dialog>
+        )
+    }
+    get drawer(){
+        return (
+            <Drawer docked={!1} onRequestChange={(drawerOpen) => this.setState({drawerOpen})} open={this.state.drawerOpen} width={300}>
+                <Link to='/'>
+                    <MenuItem>
+                        خانه
+                    </MenuItem>
+                </Link>
+            </Drawer>
+        )
+    }
     auth = validateCookie()
     render(){
         document.body.className = '';
@@ -41,97 +128,81 @@ export default class HomePage extends React.Component {
                 <Helmet>
                     <title>آچار | فروشگاه آنلاین تجهیزات و ابزار صنعتی</title>
                 </Helmet>
-                <Dialog open={this.state.searchDialogOpen} onRequestClose={this.closeSearchDialog} title='جستجو در آچار'>
-                    <form onSubmit={this.handleSearch} style={{
+                {this.bar}
+                {this.searchDialog}
+                {this.drawer}
+                <div>
+                    <Paper rounded={false} style={{
+                        backgroundColor: palette.accent1Color,
+                        backgroundImage: `linear-gradient(to right, transparent, ${palette.accent2Color})`,
+                        color: '#fff',
+                        height: 256,
                         display: 'flex',
                         alignItems: 'center',
-                        flexDirection: 'row-reverse'
+                        justifyContent: 'center'
                     }}>
-                        <IconButton type="submit" tabIndex={-1} style={{marginLeft: 8}}>
-                            <FontIcon className="mdi">search</FontIcon>
-                        </IconButton>
-                        <TextField ref='searchField' fullWidth autoComplete='off' hintText='جستجو کنید...' name='query' />
-                        <IconButton tabIndex={-1} type="button" style={{marginRight: 8}} onClick={this.closeSearchDialog}>
-                            <FontIcon className="mdi">close</FontIcon>
-                        </IconButton>
-                    </form>
-                </Dialog>
-                <div>
-                    <AppBar style={{position: 'fixed'}} iconElementLeft={<IconButton><FontIcon className='mdi' color={palette.primary3Color}>menu</FontIcon></IconButton>} zDepth={2} title='آچار' iconElementRight={<React.Fragment>
-                        <IconButton onClick={() => {
-                            this.setState({
-                                searchDialogOpen: true
-                            })
-                        }} tooltip='جستجو'>
-                            <FontIcon color={palette.primary3Color} className="mdi">search</FontIcon>
-                        </IconButton>
-                        {this.auth && <Link to='/cart'>
-                            <IconButton tooltip='مشاهده سبد خرید'>
-                                <FontIcon className='mdi' color={palette.primary3Color}>shopping_cart</FontIcon>
-                                <Badge
-                                    badgeContent={numToFA(5)}
-                                    badgeStyle={{top: -36, left: -12}}
-                                    secondary={true}>
-                                </Badge>
-                            </IconButton>
-                        </Link>
-                        }
-                        <IconMenu iconButtonElement={
-                                <IconButton tooltipPosition='bottom-left' tooltip='حساب کاربری'>
-                                    <FontIcon color={palette.primary3Color} className='mdi'>
-                                        person
-                                    </FontIcon>
-                                </IconButton>
-                            } targetOrigin={{horizontal: 'right', vertical: 'top'}} anchorOrigin={{horizontal: 'right', vertical: 'top'}}>
-                            {!this.auth ?
-                                <React.Fragment>
-                                    <Link to='/signup'>
-                                        <MenuItem primaryText='ثبت نام' />
-                                    </Link>
-                                    <Link to='/signin'>
-                                        <MenuItem primaryText="ورود به حساب" />
-                                    </Link>
-                                </React.Fragment>
-                            :
-                            <React.Fragment>
-                                <Link to='/account/logout'>
-                                    <MenuItem primaryText='خروج از حساب' />
+                        <div>
+                            <big>
+                                <h1>آچار</h1>
+                            </big>
+                            <b>فروشگاه آنلاین ابزار و تجهیزات صنعتی</b>
+                            <br />
+                            <Space height={8} />
+                            <div style={{
+                                textAlign: 'center'
+                            }}>
+                                <RaisedButton label="مشاهده دسته بندی ها"></RaisedButton>
+                                <Link to="/signup">
+                                    <RaisedButton primary style={{marginLeft: 4}} label="ثبت نام در آچار"></RaisedButton>
                                 </Link>
-                                <Link to='/account/delete-account'>
-                                    <MenuItem primaryText='حذف حساب کاربری' />
-                                </Link>
-                            </React.Fragment>
-                            }
-                        </IconMenu>
-                    </React.Fragment>} onLeftIconButtonClick={() => this.setState(prev => ({drawerOpen: true}))} />
-                    <Drawer docked={!1} onRequestChange={(drawerOpen) => this.setState({drawerOpen})} open={this.state.drawerOpen} width={300}>
-                        <Link to='/'>
-                            <MenuItem>
-                                خانه
-                            </MenuItem>
-                        </Link>
-                    </Drawer>
-                    <Space height={56 + 16} />
-                    <MulitshSelf icon='bookmark border' headerText='جدیدترین کالاها' interval={7000}>
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                    </MulitshSelf>
-                    <MulitshSelf icon='star border' headerText='پرفروش ترین ها' interval={8500}>
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                        <MulitshItem title='کالای اول' description='کالای دوم' imagePath={{}} price={41000} />
-                    </MulitshSelf>
+                            </div>
+                        </div>
+                    </Paper>
+                    <div className="container">
+                        <div className="col-xs-12 col-md-8" style={{
+                            float: 'none',
+                            margin: '0 auto',
+                            padding: 16
+                        }}>
+                            <div style={{
+                                textAlign: 'center'
+                            }}>
+                                <h1>آچار چیست؟</h1>
+                                <p className="mute">
+                                    آچار یک فروشگاه آنلاین تجهیزات و ابزار پیشرفته صنعتی است. خب گیتااار.
+                                </p>
+                                <h1>آچار نسبت به دیگران چه برتری هایی دارد؟</h1>
+                                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 landing-intro-section">
+                                    <i className="mdi" style={{color: palette.primary2Color}}>search</i>
+                                    <div className="title">آچار جــالب است.</div>
+                                    <p>
+                                        جالبه دیگه. جـــــالب.
+                                    </p>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 landing-intro-section">
+                                    <i className="mdi" style={{color: palette.primary2Color}}>search</i>
+                                    <div className="title">آچار جــالب است.</div>
+                                    <p>
+                                        جالبه دیگه. جـــــالب.
+                                    </p>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 landing-intro-section">
+                                    <i className="mdi" style={{color: palette.primary2Color}}>search</i>
+                                    <div className="title">آچار جــالب است.</div>
+                                    <p>
+                                        جالبه دیگه. جـــــالب.
+                                    </p>
+                                </div>
+                                <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 landing-intro-section">
+                                    <i className="mdi" style={{color: palette.primary2Color}}>search</i>
+                                    <div className="title">آچار جــالب است.</div>
+                                    <p>
+                                        جالبه دیگه. جـــــالب.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         )
