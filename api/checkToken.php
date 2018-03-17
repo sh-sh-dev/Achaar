86aa87dfdeb4dae29f8b995105c281abb7100a1e
Shaygan Shokrollahi <shayganshok0@gmail.com>
2018-03-17 23:00:20 +0330

    🐛 checkToken

--- api/checkToken.php
+++ api/checkToken.php
@@ -6,3 +6,7 @@ $Data = getData();
 $Token = $db->real_escape_string(Clean($Data["token"]));
 
 if (!CheckToken($Token)) die(Response("توکن معتبر نیست",false,-2));
+
+else {
+    Response("توکن معتبر است",true,null,true);
+}
